0e0358b471257175971b8a6c58f1f730823a83ed
Pamui89 <pablo.m@ur-fixer.com>
2024-04-08 10:24:22 +0100

    Implementing subfolder search and categories field

--- collect_gpx_data.py
+++ collect_gpx_data.py
@@ -16,25 +16,26 @@ def parse_gpx(file_path):
         for track in gpx.tracks:
             for segment in track.segments:
                 competitor_id = os.path.splitext(os.path.basename(file_path))[0]
-                start_time = datetime.now()
-                finish_time = datetime.now()
-                # start_time = segment.points[0].time.replace(tzinfo=None)
-                # finish_time = segment.points[-1].time.replace(tzinfo=None)
+                category = os.path.basename(os.path.dirname(file_path))
+                # start_time = datetime.now()
+                # finish_time = datetime.now()
+                start_time = segment.points[0].time.replace(tzinfo=None)
+                finish_time = segment.points[-1].time.replace(tzinfo=None)
                 distance2d = segment.length_2d()/1000  # Distance in kilometers
                 distance3d = segment.length_3d()/1000  # Distance in kilometers
                 elapsed_time = (finish_time - start_time).total_seconds()
                 
-                yield competitor_id, start_time, finish_time, elapsed_time, distance2d, distance3d
+                yield competitor_id, category, start_time, finish_time, elapsed_time, distance2d, distance3d
 
 timestamp_str = datetime.now().strftime('%Y%m%d_%H%M%S')
-output_file = f'/Users/pamui/Documents/Developer/PythonPrograms/collect-gpx-data/results/race_results_{timestamp_str}.xlsx'
+output_file = f'/Users/pamui/Documents/Developer/PythonPrograms/collect_gpx_data/results/race_results_{timestamp_str}.xlsx'
 def write_to_excel(data, output_file=output_file):
     wb = Workbook()
     ws = wb.active
     ws.title = "Race Data"
 
     # Headers
-    ws.append(['Competitor Id', 'Start Time', 'Finish Time', 'Total time', 'Total Distance 2d', 'Total Distance 3d'])
+    ws.append(['Competitor Id', 'Category', 'Start Time', 'Finish Time', 'Total time', 'Total Distance 2d', 'Total Distance 3d'])
 
     # Data
     for row in data:
@@ -42,7 +43,7 @@ def write_to_excel(data, output_file=output_file):
 
     # Formatting duration as [h]:mm:ss
     duration_style = NamedStyle(name='duration', number_format='[h]:mm:ss')
-    for cell in ws['D'][1:]:  # Column D contains durations in seconds
+    for cell in ws['E'][1:]:  # Column E contains durations in seconds
         # Convert seconds to Excel time format (fraction of 24 hours)
         cell.value = cell.value / 86400  # There are 86400 seconds in a day
         cell.style = duration_style
@@ -50,7 +51,7 @@ def write_to_excel(data, output_file=output_file):
     wb.save(output_file)
 
 # Path where your GPX files are stored
-gpx_files = glob.glob('/Users/pamui/Documents/Developer/PythonPrograms/collect-gpx-data/assets/*.gpx')
+gpx_files = glob.glob(f'/Users/pamui/Documents/Developer/PythonPrograms/collect_gpx_data/assets/**/*.gpx', recursive=True)
 
 # Collect data from each file
 all_data = []
