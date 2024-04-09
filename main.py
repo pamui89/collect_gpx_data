9a67caa5baa6057b93e11600d7ac0ae3a511cdf0
Pamui89 <pablo.m@ur-fixer.com>
2024-04-09 09:24:58 +0100

    Getting personal info, penalties and compensations from competitors file

--- main.py
+++ main.py
@@ -25,6 +25,22 @@ results_table_name = config['excel_params']['table_name']
 results_sheet_name = config['excel_params']['sheet_name']
 results_table_style_name = config['excel_params']['table_style_name']
 
+def get_competitor_data(competitor_id):
+    row = competitors_data[competitors_data['competitor_id'] == competitor_id]
+    if not row.empty:
+        team = row.iloc[0]['team']
+        name = row.iloc[0]['name']
+        surname = row.iloc[0]['surname']
+        bike_brand = row.iloc[0]['bike_brand']
+        bike_model = row.iloc[0]['bike_model']
+        category = row.iloc[0]['category']
+        gas_compensation = 0
+        chicken_way_penalty = 0
+        if row.iloc[0]['gas_compensation'] == True: gas_compensation = 6
+        if row.iloc[0]['chicken_way_penalty'] == True: chicken_way_penalty = 1
+        return team, name, surname, bike_brand, bike_model, category, gas_compensation, chicken_way_penalty
+    return None, None, None, None, None, None, None, None
+
 def get_reference_data(category):
     row = reference_data[reference_data['category'] == category]
     if not row.empty:
@@ -37,7 +53,7 @@ def parse_reference_gpx(file_path):
         
         for track in gpx.tracks:
             for segment in track.segments:
-                category = os.path.basename(os.path.dirname(file_path))
+                category = os.path.splitext(os.path.basename(file_path))[0]
                 min_distance2d = segment.length_2d()/1000  # Distance in kilometers
                 
                 yield category, min_distance2d
@@ -49,7 +65,7 @@ def parse_gpx(file_path):
         for track in gpx.tracks:
             for segment in track.segments:
                 competitor_id = os.path.splitext(os.path.basename(file_path))[0]
-                category = os.path.basename(os.path.dirname(file_path))
+                team, name, surname, bike_brand, bike_model, category, gas_compensation, chicken_way_penalty = get_competitor_data(competitor_id)
                 if segment.points[0].time is None:
                     start_time = datetime.now()
                     finish_time = datetime.now()
@@ -57,16 +73,17 @@ def parse_gpx(file_path):
                     start_time = segment.points[0].time.replace(tzinfo=None)
                     finish_time = segment.points[-1].time.replace(tzinfo=None)
                 distance2d = segment.length_2d()/1000  # Distance in kilometers
+                adjusted_distance = distance2d - gas_compensation + chicken_way_penalty
                 min_distance2d = get_reference_data(category)
                 distance_ok = True
-                distance_diference = distance2d - min_distance2d
-                if distance2d < min_distance2d: distance_ok = False
+                distance_diference = adjusted_distance - min_distance2d
+                if adjusted_distance < min_distance2d: distance_ok = False
                 elapsed_time = (finish_time - start_time).total_seconds()
                 no_time = False
                 if elapsed_time < 1: 
                     no_time = True
                 
-                yield competitor_id, category, start_time, finish_time, elapsed_time, distance2d, min_distance2d, distance_diference, distance_ok, no_time
+                yield competitor_id, team, name, surname, bike_brand, bike_model, category, start_time, finish_time, elapsed_time, distance2d, min_distance2d, gas_compensation, chicken_way_penalty, adjusted_distance, distance_diference, distance_ok, no_time
 
 timestamp_str = datetime.now().strftime('%Y%m%d_%H%M%S')
 output_file = f'{results_output_folder}/race_results_{timestamp_str}.xlsx'
@@ -76,7 +93,7 @@ def write_to_excel(data, output_file=output_file):
     ws.title = results_sheet_name
 
     # Headers
-    ws.append(['Competitor Id', 'Category', 'Start Time', 'Finish Time', 'Total time', 'Total Distance 2d', 'Min Distance 2d', 'Distance Difference', 'Distance Ok', 'No Time'])
+    ws.append(['Competitor Id', 'Team', 'Name', 'Surname', 'Bike Brand', 'Bike Model', 'Category', 'Start Time', 'Finish Time', 'Total time', 'Total Distance 2d', 'Min Distance 2d', 'Gas Compensation', 'Chicken Way Penalty', 'Adjusted Total Distance', 'Distance Difference', 'Distance Ok', 'No Time'])
 
     # Total time column
     total_time_column = None
@@ -115,8 +132,9 @@ def write_to_excel(data, output_file=output_file):
     wb.save(output_file)
 
 # Collect data from competitors file
-competitors_data = pd.read_excel(competitors_file, sheet_name=competitors_sheet, usecols='A:G')
+competitors_data = pd.read_excel(competitors_file, sheet_name=competitors_sheet, usecols='A:I', dtype={'category': str})
 competitors_data = pd.DataFrame(competitors_data)
+print(competitors_data)
 
 # Path where reference GPX files are stored
 reference_gpx_files = glob.glob(f'{reference_distance_folder}/**/*.gpx', recursive=True)
@@ -137,7 +155,7 @@ all_data = []
 for file_path in tqdm(gpx_files, desc= 'Processing competitors GPX Files'):
     all_data.extend(parse_gpx(file_path))
 
-all_data = pd.DataFrame(all_data, columns=['competitor_id', 'category', 'start_time', 'finish_time', 'elapsed_time', 'distance2d', 'min_distance2d', 'distance_diference', 'distance_ok', 'no_time'])
+all_data = pd.DataFrame(all_data, columns=['competitor_id', 'team', 'name', 'surname', 'bike_brand', 'bike_model', 'category', 'start_time', 'finish_time', 'elapsed_time', 'distance2d', 'min_distance2d', 'gas_compensation', 'chicken_way_penalty', 'adjusted_distance', 'distance_diference', 'distance_ok', 'no_time'])
 
 # Write the collected data to an Excel file
 write_to_excel(all_data)
